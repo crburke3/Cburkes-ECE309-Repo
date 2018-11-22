4dae967216815c1eeeefe74a12fde9411aaf6a96
crburke3 <43005219+crburke3@users.noreply.github.com>
2018-11-21 19:51:12 -0500

    ShortestPathTester

---
+++ tests/ShortestPathTester.cpp
@@ -0,0 +1,45 @@
+//
+//  ShortestPathTester.cpp
+//  ECE309Tester
+//
+//  Created by Christian Burke on 11/21/18.
+//  Copyright © 2018 Christian Burke. All rights reserved.
+//
+
+#include "ShortestPath.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <set>
+
+int main() {
+    
+    int size = 10;
+    int *array = new int[size];
+    printf("Before: ");
+    for (int i = 0; i < size; i++) {
+        array[i] = 2 * i;
+        printf("%d ", array[i]);
+    }
+    
+    MaxHeap::sort(array, size);
+    
+    int* minHeap = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        minHeap[i] = array[(size - 1) - i];
+    }
+    
+    printf("\nAfter: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", array[i]);
+    }
+    
+    
+    node startNode(0, array[0]);
+    node endNode(9, array[9]);
+    std::set<node> shortestSet;
+    
+    shortestPath(array, size, startNode, endNode);
+    
+    return 0;
+}
